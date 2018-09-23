6b02fd5c5e67fcda93abc085c7bba3f3f83123af
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-09-23 22:42:08 +0300

    Class MethodInfo updated

--- TracerDemonstration/Tracer/TraceResult.cs
+++ TracerDemonstration/Tracer/TraceResult.cs
@@ -9,13 +9,53 @@ namespace Tracer
 {
     public class MethodInfo
     {
-        private List<MethodInfo> inMethods;
         private Stopwatch timer;
+        private List<MethodInfo> insideMethods;        
+
+        public MethodInfo()
+        {
+            timer = new Stopwatch();
+            insideMethods = new List<MethodInfo>();           
+        }
 
         public string Name { get; set; }
         public string ClassName { get; set; }
-        public string Time { get; set; }
+
+        public long Time
+        {
+            get
+            {
+                return timer.ElapsedMilliseconds;
+            }
+        }
+
+        public string TimeStr { get { return Time.ToString() + "ms"; } set { } }
+
+        public List<MethodInfo> InsideMethods
+        {
+            get
+            {
+                return new List<MethodInfo>(insideMethods);
+            }
+            set { }
+        }        
+
+        internal void StartTrace()
+        {
+            timer.Start();
+        }
+
+        internal void StopTrace()
+        {
+            timer.Stop();
+        }
+
+        internal void InsideMethodAdd(MethodInfo method)
+        {
+            insideMethods.Add(method);
+        }
     }
+
 
     public class TraceResult
     {
