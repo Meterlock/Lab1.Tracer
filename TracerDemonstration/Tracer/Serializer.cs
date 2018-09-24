ec35542bdefa62925bdef1d4ed681b9960bf7932
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-09-24 14:50:15 +0300

    Implemented Json serializer

---
+++ TracerDemonstration/Tracer/Serializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace Tracer
+{
+    public class SerializerJSON : ISerializer
+    {
+        public void Serialize(Stream stream, TraceResult traceResult)
+        {
+            var jsonFormatter = new DataContractJsonSerializer(typeof(TraceResult));
+            using (stream)
+            {
+                jsonFormatter.WriteObject(stream, traceResult);
+            }
+        }
+    }
+
+    public class SerializerXML : ISerializer
+    {
+        public void Serialize(Stream stream, TraceResult traceResult)
+        {
+
+        }
+    }
+}
